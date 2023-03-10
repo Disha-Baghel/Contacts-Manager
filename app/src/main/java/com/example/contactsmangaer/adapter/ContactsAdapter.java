7f8c92fad3caf1dd4d26e92bbab82c8610c1f3d1
Disha-Baghel <bagheldisha708@gmail.com>
2023-03-10 20:56:59 +0530

    Added new files

--- app/src/main/java/com/example/contactsmangaer/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/contactsmangaer/adapter/ContactsAdapter.java
@@ -1,2 +1,47 @@
-package com.example.contactsmangaer.adapter;public class ContactsAdapter {
+package com.example.contactsmangaer.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactsmangaer.MainActivity;
+import com.example.contactsmangaer.R;
+import com.example.contactsmangaer.db.entity.Contact;
+
+import java.util.ArrayList;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+
+    //1 - Variable
+    private Context context;
+    private ArrayList<Contact> contactsList;
+    private MainActivity mainActivity;
+
+    //ViewHolder
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView name;
+        public TextView email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.name = itemView.findViewById(R.id.name);
+            this.email = itemView.findViewById(R.id.email);
+        }
+    }
+
+    public ContactsAdapter(Context context, ArrayList<Contact> contacts, MainActivity mainActivity){
+        this.context = context;
+        this.contactsList = contacts;
+        this.mainActivity = mainActivity;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itenView =
+    }
 }
